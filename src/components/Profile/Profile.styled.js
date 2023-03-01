5e0ef988d677a042b5488b0adf7ae7d673ab8d57
SvitlanaPia <111311701+SvitlanaPia@users.noreply.github.com>
2023-03-01 20:03:51 +0200

    first-task

---
+++ src/components/Profile/Profile.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  background: white;
+  margin: 0 auto;
+`;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    backgraund: '#E7ECF1',
+    while: '#FFFFFF',
+    mainText: '#122335',
+    text: '#E4E7EA',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
